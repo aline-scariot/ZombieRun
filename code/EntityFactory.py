19831fecbcf63adc96a997f232b63d57e01c9fcf
aline <alinessommer@gmail.com>
2026-03-23 01:58:38 -0300

    Creating asset folder Inserting all assets Creating the menu background, sound and fonts Creating the const file

---
+++ code/EntityFactory.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from code.Background import Background
+
+class EntityFactory:
+    @staticmethod
+    def get_entity(entity_name: str, position=(0,0)):
+        match entity_name:
+            case 'Bg':
+                list_bg = []
+                for i in range(5):
+                    list_bg.append(Background(f'Bg{i}', (0,0)))
+                return list_bg
+
+
--- code/Game.py
+++ code/Game.py
@@ -1,24 +1,30 @@
     #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
-
+from code.Const import WIN_HEIGHT, WIN_WIDTH, MENU_OPT
+from code.Level import Level
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(700, 520))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
+
 
     def run(self, ):
-        pass
 
         while True:
             menu = Menu(self.window)
-            menu.run()
-            pass
-            # for event in pygame.event.get():
-            #    if event.type == pygame.QUIT:
-            #        pygame.quit()
-            #        quit() #end pygame
+            menu_return = menu.run()
+
+            if menu_return == MENU_OPT[0]:
+                level = Level(self.window, 'Bg', menu_return)
+                level_return = level.run()
+            elif menu_return == MENU_OPT[1]:
+                pygame.quit()
+                quit()
+            else:
+                pass
+
 
